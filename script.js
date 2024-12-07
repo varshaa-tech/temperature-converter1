3be1ccebb081e7acc2ad90a295a39315444ae23b
Varsha <varsha040219@gmail.com>
2024-12-07 11:35:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function converttemperature(){
+    var temperatureInput=document.getElementById("temperature").value;
+    var fromUnit=document.getElementById("from").value;
+    var toUnit=document.getElementById("To").value;
+
+    if (isNaN(temperatureInput)) {
+        alert("Please enter a valid temperature.");
+        return;
+      } 
+       var temperature=parseFloat(temperatureInput);
+       var convertedTemperature;
+       if(fromUnit === "celsius") {
+        if (toUnit === "fahrenheit") {
+          convertedTemperature = (temperature * 9 / 5) + 32;
+        } else if (toUnit === "kelvin") {
+          convertedTemperature = temperature + 273.15;
+        } else {
+          convertedTemperature = temperature;
+        }
+      } else if (fromUnit === "fahrenheit") {
+        if (toUnit === "celsius") {
+          convertedTemperature = (temperature - 32) * 5 / 9;
+        } else if (toUnit === "kelvin") {
+          convertedTemperature = (temperature + 459.67) * 5 / 9;
+        } else {
+          convertedTemperature = temperature;
+        }
+      } else if (fromUnit === "kelvin") {
+        if (toUnit === "celsius") {
+          convertedTemperature = temperature - 273.15;
+        } else if (toUnit === "fahrenheit") {
+          convertedTemperature = (temperature * 9 / 5) - 459.67;
+        } else {
+          convertedTemperature = temperature;
+        }
+      }
+      console.log(convertedTemperature);
+var resultElement = document.getElementById("result");
+resultElement.textContent = convertedTemperature.toFixed(2) + " " + toUnit;
+  
+}
